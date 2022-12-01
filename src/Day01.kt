aceb7dbcdd2ff4292f85c8fc66ea6b9d85fc7a05
Tobias Jahn <tobias.jahn@totwart.de>
2022-12-01 23:37:41 +0100

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,34 @@
 fun main() {
+    fun getCalsByElves(input: List<String>) : List<Int>{
+        var numberInput = input.map { if (it.isEmpty()) -1 else it.toInt() }.toList()
+
+        val emptyLines = numberInput.mapIndexed { index, i -> if (i == -1) index + 1 else -1 }.filter { it != -1 }.toMutableList().dropLast(0).reversed().toMutableList()
+        emptyLines.add(0)
+
+        val resultSet = emptyLines.map {
+            val result = numberInput.takeLast(numberInput.size - it).sum()
+            numberInput = numberInput.dropLast(numberInput.size - it).dropLast(1)
+            result
+        }.toList()
+
+        return resultSet
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return getCalsByElves(input).max()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return getCalsByElves(input).sortedDescending().take(3).sum()
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(readInput("Day01_test")) == 24000)
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    println(part1(readInput("Day01_test")))
+    println(part1(readInput("Day01")))
+
+    check(part2(readInput("Day01_test")) == 45000)
+
+    println(part2(readInput("Day01_test")))
+    println(part2(readInput("Day01")))
 }
