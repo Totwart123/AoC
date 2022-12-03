b131e4b2fc11fd9d86b9bc2b54a7d5a5449cd263
Tobias Jahn <tobias.jahn@totwart.de>
2022-12-03 11:29:54 +0100

    Day 3 solved

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,18 +1,36 @@
 fun main() {
+    fun Char.getValueOfItem() = if(this.isUpperCase()) this.code - 38 else this.code - 96
+
     fun part1(input: List<String>): Int {
-        return 0
+        var sum = 0
+        input.forEach {items ->
+            val compartment1 = items.substring(0, items.length / 2)
+            val compartment2 = items.substring(items.length/2, items.length)
+
+            val double = compartment1.first { compartment2.contains(it) }
+
+            sum += double.getValueOfItem()
+        }
+
+        return sum
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        var sum = 0
+        input.chunked(3).forEach { group ->
+            val itemType =  group[0].first { group[1].contains(it) && group[2].contains(it) }
+
+            sum += itemType.getValueOfItem()
+        }
+        return sum
     }
 
     val testInput = readInput("Day03_test")
-    check(part1(testInput) == 15)
-    check(part2(testInput) == 12)
+    check(part1(testInput) == 157)
+    check(part2(testInput) == 70)
 
-    val input = readInput("Day03")
-    println(part1(input))
-    println(part2(input))
+     val input = readInput("Day03")
+     println(part1(input))
+     println(part2(input))
 
 }
---
+++ src/Dayxx.kt
@@ -0,0 +1,17 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        return 0
+    }
+
+    fun part2(input: List<String>): Int {
+        return 0
+    }
+
+    val testInput = readInput("Dayxx_test")
+    check(part1(testInput) == 0)
+    //check(part2(testInput) == 0)
+
+//     val input = readInput("Dayxx")
+//     println(part1(input))
+//     println(part2(input))
+}
