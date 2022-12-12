3c9ef28bd4ec55000baa51d44eeaf5b9478217b1
Tobias Jahn <tobias.jahn@totwart.de>
2022-12-13 00:18:35 +0100

    Day12

---
+++ src/Day12.kt
@@ -0,0 +1,18 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+
+        return 0
+    }
+
+    fun part2(input: List<String>): Int {
+        return 0
+    }
+
+    val testInput = readInput("Day12_test")
+    check(part1(testInput) == 31)
+    //check(part2(testInput) == 0)
+
+    val input = readInput("Day12")
+    println(part1(input))
+//     println(part2(input))
+}
